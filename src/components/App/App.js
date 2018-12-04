da17313d38ddc36ffb1599723ebf64897c58a048
JTimerman <joaquin.timerman@gmail.com>
2018-12-04 15:06:54 -0300

    Added view Home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Location from "../Location/Location.js";
 import Header from "../Header/Header.js";
+import Home from "../../views/Home/Home.js";
 import "./App.css";
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Header />
-                <Location />
+                <Home />
             </div>
         );
     }
---
+++ src/views/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import Location from "../../components/Location/Location.js";
+
+const Header = () => {
+    return (
+        <main>
+            <Location />
+        </main>
+    );
+};
+
+export default Header;
