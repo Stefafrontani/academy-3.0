7d8dc5fde3fbbbdbbe1e70c02c553c5de2f57b89
JTimerman <joaquin.timerman@gmail.com>
2018-12-04 16:24:19 -0300

    Added Analytics Graph with AntDesignPro

--- src/views/Analytics.js
+++ src/views/Analytics.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Bar } from "ant-design-pro/lib/Charts";
+
+const salesData = [];
+for (let i = 0; i < 23; i += 1) {
+    salesData.push({
+        x: `${i + 1}A`,
+        y: Math.floor(Math.random() * 1000) + 200
+    });
+}
 
 const Analytics = () => {
-    return <>Analytics</>;
+    return <Bar height={200} title="Humedad" data={salesData} />;
 };
 
 export default Analytics;
