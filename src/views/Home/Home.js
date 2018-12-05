1507423f25449431e3dd6f9a7c2005ee90bd0098
JTimerman <joaquin.timerman@gmail.com>
2018-12-05 16:44:04 -0300

    Added changes to db.json

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -12,18 +12,12 @@ const datalist = [
     "Entre Rios"
 ];
 
-const toast = {
-    show: true,
-    message: "Fuertes tormentas al Sur de la Capital Federal",
-    icon: "./assets/images/warning.svg"
-};
-
 const Home = () => {
     return (
         <main className="home">
             <InputWithDatalist datalist={datalist} placeholder="Location..." />
             <WeatherCards />
-            <Toast {...toast} />
+            <Toast />
         </main>
     );
 };
